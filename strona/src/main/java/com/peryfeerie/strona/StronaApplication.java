7eca30789667d6f5e4f34271912e842715dc91ab
ngramont <natanaelgramont@gmail.com>
2026-02-19 16:32:17 +0100

    initial commit

---
+++ strona/src/main/java/com/peryfeerie/strona/StronaApplication.java
@@ -0,0 +1,13 @@
+package com.peryfeerie.strona;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StronaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StronaApplication.class, args);
+	}
+
+}
